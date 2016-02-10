36867d7d978daa0fe1d8f446790b6db726d586a3
MaithiliJampana <maithili.jampana@gmail.com>
2016-02-10 14:54:22 +0530

    changes

--- citeis-auto/src/test/java/com/cisco/citeis/scripts/VerifyAppFlow.java
+++ citeis-auto/src/test/java/com/cisco/citeis/scripts/VerifyAppFlow.java
@@ -12,7 +12,7 @@ import com.cisco.citeis.util.PropertyUtil;
 
 public class VerifyAppFlow extends TestBase{
 
-	@BeforeMethod
+	@BeforeMethod(alwaysRun=true)
 	public void initialize(){
 		driver.get(PropertyUtil.configMap.get("URL"));
 		driver.manage().window().maximize();
@@ -20,33 +20,33 @@ public class VerifyAppFlow extends TestBase{
 	}
 	
 	@Test(groups={"SanityTest","ExtendedSanityTest"})
-	public void validateMyApplications(){
-		appStartPage.checkPageLoaded();
-		homePage = appStartPage.clickManageBtn();
-		myApplicationsPage = homePage.clickMyApplications();
-	}
-	
-	@Test(groups={"SanityTest","ExtendedSanityTest"})
 	@Parameters({"application","profile","epg"})
 	public void validatePositiveFlow(String application,String profile,String epg) {
-		myApplicationsPage.validateAppFlow(application,profile,epg);
+		validateFlow(application, profile, epg);
 	}
 	
 	@Test(groups={"SanityTest","ExtendedSanityTest"})
 	@Parameters({"wrongapplication","profile","epg"})
 	public void validateWrongApplication(String application,String profile,String epg) {
-		myApplicationsPage.validateAppFlow(application,profile,epg);
+		validateFlow(application, profile, epg);
 	}
 	
 	@Test(groups={"ExtendedSanityTest"})
 	@Parameters({"application","wrongprofile","epg"})
 	public void validateWrongProfile(String application,String profile,String epg) {
-		myApplicationsPage.validateAppFlow(application,profile,epg);
+		validateFlow(application, profile, epg);
 	}
 	
 	@Test(groups={"ExtendedSanityTest"})
 	@Parameters({"application","profile","wrongepg"})
 	public void validateWrongEPG(String application,String profile,String epg) {
+		validateFlow(application, profile, epg);
+	}
+	
+	private void validateFlow(String application,String profile,String epg){
+		appStartPage.checkPageLoaded();
+		homePage = appStartPage.clickManageBtn();
+		myApplicationsPage = homePage.clickMyApplications();
 		myApplicationsPage.validateAppFlow(application,profile,epg);
 	}
 	
